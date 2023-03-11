21253e3b16ddc05150848ced20851781ef9cff8e
jeongseungmin <jsm8109jsm@gmail.com>
2023-03-11 11:23:43 +0900

    FEAT :: Header Setting button, Refactoring

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 import React from "react";
+import { Settings } from "@mui/icons-material";
 
 function Header() {
   return (
-    <header className="bg-black p-[5px] h-[54px] letter flex items-center">
-      <Link href='/' className="h-full inline-flex items-center text-textLightGray text-xl font-bold hover:bg-grayHover tracking-tight px-[1.8rem]">BSM Deploy</Link>
+    <header className="bg-black p-[5px] h-[54px] flex items-center gap-2">
+      <Link
+        href="/"
+        className="header-button text-[2rem] font-bold h-full tracking-tight px-[1.8rem]"
+      >
+        BSM Deploy
+      </Link>
+      <div className="!w-[4.4rem] !h-[4.4rem] header-button">
+        <Settings className="text-textLightGray !text-5xl" />
+      </div>
     </header>
   );
 }
