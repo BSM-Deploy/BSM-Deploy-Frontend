de98559bf9d85c8eb9a51c1d83aa00849406539d
jeongseungmin <jsm8109jsm@gmail.com>
2023-03-17 13:07:43 +0900

    FEAT :: 아이콘, 배포 여부 툴팁

--- components/layout/section/project/ProjectItem.tsx
+++ components/layout/section/project/ProjectItem.tsx
@@ -2,16 +2,27 @@ import MultipleHtml from "@/components/icons/MultipleHtml";
 import NextIcon from "@/components/icons/NextIcon";
 import ReactIcon from "@/components/icons/ReactIcon";
 import SingleHtml from "@/components/icons/SingleHtml";
+import { MdClear, MdCheck } from "react-icons/md";
 import { ProjectType } from "@/types/project";
+import { Tooltip } from "@mui/material";
 import React from "react";
 
 function ProjectItem({ data }: { data: ProjectType }) {
-  const iconObject = {
-    SINGLE_HTML: <SingleHtml />,
-    MULTIPLE_FILE: <MultipleHtml />,
-    BUILT_REACT_JS: <ReactIcon />,
-    BUILT_NEXT_JS: <NextIcon />,
+  const projectType = {
+    icon: {
+      SINGLE_HTML: <SingleHtml />,
+      MULTIPLE_FILE: <MultipleHtml />,
+      BUILT_REACT_JS: <ReactIcon />,
+      BUILT_NEXT_JS: <NextIcon />,
+    },
+    tooltip: {
+      SINGLE_HTML: "단일 HTML",
+      MULTIPLE_FILE: "다중 파일",
+      BUILT_REACT_JS: "React.js",
+      BUILT_NEXT_JS: "Next.js",
+    },
   };
+  const { icon, tooltip } = projectType;
   return (
     <div className="h-80 w-80 dark:bg-itemGray p-8 rounded-2xl border border-solid border-lightBack relative bg-textLightGray">
       <h1 className="m-0 text-4xl dark:text-textLightGray font-bold">
@@ -20,9 +31,24 @@ function ProjectItem({ data }: { data: ProjectType }) {
       <span className="text-xl text-textDarkGray">
         {data.domainPrefix}.bssm.hs.kr
       </span>
-      <div className="bg-textDarkGray dark:bg-textLightGray rounded-full w-12 h-12 absolute bottom-8 inline-flex justify-center items-center left-8">
-        {iconObject[data.projectType]}
-      </div>
+      <Tooltip title={tooltip[data.projectType]} arrow>
+        <div className="bg-textDarkGray dark:bg-textLightGray rounded-full w-12 h-12 absolute bottom-8 inline-flex justify-center items-center left-8">
+          {icon[data.projectType]}
+        </div>
+      </Tooltip>
+      {data.isDeploy ? (
+        <Tooltip title="배포됨" arrow>
+          <div className="absolute bottom-8 right-8">
+            <MdCheck className="text-4xl dark:text-textLightGray text-textDarkGray" />
+          </div>
+        </Tooltip>
+      ) : (
+        <Tooltip title="배포되지 않음" arrow>
+          <div className="absolute bottom-8 right-8">
+            <MdClear className="text-4xl dark:text-textLightGray text-textDarkGray" />
+          </div>
+        </Tooltip>
+      )}
     </div>
   );
 }
