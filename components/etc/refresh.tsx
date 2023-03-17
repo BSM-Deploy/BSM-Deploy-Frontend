923b880ba6bdbd533c915304a1a3741682799793
jeongseungmin <jsm8109jsm@gmail.com>
2023-03-17 11:20:24 +0900

    FEAT :: File별 아이콘

--- components/etc/refresh.tsx
+++ components/etc/refresh.tsx
@@ -13,16 +13,11 @@ export const Refresh = () => {
     const _err = err as unknown as AxiosError;
     const { response } = _err;
     const originalConfig = _err?.config;
-    console.log(localStorage.refreshToken);
     if (localStorage.refreshToken && response && response.status === 401) {
-      console.log("asdf");
-      const refreshToken = localStorage.refreshToken;
       refreshMutation.mutate();
       if (refreshMutation.isSuccess) {
-        console.log(refreshMutation);
         localStorage.setItem("accessToken", refreshMutation.data.accessToken);
       } else if (refreshMutation.isError) {
-        console.log(refreshMutation);
         localStorage.removeItem("accessToken");
         localStorage.removeItem("refreshToken");
       }
--- components/layout/section/project/ProjectItem.tsx
+++ components/layout/section/project/ProjectItem.tsx
@@ -1,16 +1,28 @@
+import MultipleHtml from "@/components/icons/MultipleHtml";
+import NextIcon from "@/components/icons/NextIcon";
+import ReactIcon from "@/components/icons/ReactIcon";
+import SingleHtml from "@/components/icons/SingleHtml";
 import { ProjectType } from "@/types/project";
 import React from "react";
 
 function ProjectItem({ data }: { data: ProjectType }) {
+  const iconObject = {
+    SINGLE_HTML: <SingleHtml />,
+    MULTIPLE_FILE: <MultipleHtml />,
+    BUILT_REACT_JS: <ReactIcon />,
+    BUILT_NEXT_JS: <NextIcon />,
+  };
   return (
-    <div className="h-80 w-80 bg-itemGray p-8 rounded-2xl border border-solid border-lightBack relative">
+    <div className="h-80 w-80 dark:bg-itemGray p-8 rounded-2xl border border-solid border-lightBack relative bg-textLightGray">
       <h1 className="m-0 text-4xl dark:text-textLightGray font-bold">
         {data.name}
       </h1>
-      <span className="text-xl dark:text-textDarkGray">
+      <span className="text-xl text-textDarkGray">
         {data.domainPrefix}.bssm.hs.kr
       </span>
-      <div className="dark:bg-textLightGray rounded-full w-12 h-12 absolute bottom-8 inline-block left-8"></div>
+      <div className="bg-textDarkGray dark:bg-textLightGray rounded-full w-12 h-12 absolute bottom-8 inline-flex justify-center items-center left-8">
+        {iconObject[data.projectType]}
+      </div>
     </div>
   );
 }
