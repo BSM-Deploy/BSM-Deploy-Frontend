3d2373adda00f57bb6ee77791fa6c7f392aff346
= <=>
2023-04-20 17:12:42 +0900

    FEAT :: NextSeo 설정

--- pages/deploy/[index].tsx
+++ pages/deploy/[index].tsx
@@ -2,10 +2,18 @@ import Header from "@/components/layout/header/Header";
 import DeploySection from "@/components/layout/section/deploy/deploy";
 import Sidebar from "@/components/layout/sidebar/Sidebar";
 import ErrorSnackbar from "@/components/snackbar/errorSnackbar";
+import { NextSeo, NextSeoProps } from "next-seo";
 
 export default function Deploy() {
+
+  const seoConfig: NextSeoProps = {
+    title: "배포하기",
+    description: "프로젝트를 배포하는 페이지입니다.",
+  };
+
   return (
     <>
+    <NextSeo {...seoConfig} />
       <Header />
       <Sidebar />
       <DeploySection />
--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/layout/header/Header";
 import ProjectSection from "@/components/layout/section/project/Project";
 import Sidebar from "@/components/layout/sidebar/Sidebar";
+import { NextSeoProps, NextSeo } from "next-seo";
 import React from "react";
 
 function Project() {
+  const seoConfig: NextSeoProps = {
+    title: "프로젝트",
+    description: "프로젝트들을 모아보는 페이지입니다.",
+  };
   return (
     <>
+    <NextSeo {...seoConfig} />
       <Header title='내 프로젝트'/>
       <Sidebar />
       <ProjectSection />
