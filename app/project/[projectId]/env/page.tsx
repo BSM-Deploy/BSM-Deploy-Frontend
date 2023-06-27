1e4533a9f69ba7083d7e6bebcc909612836c55ba
= <=>
2023-06-27 12:07:07 +0900

    FIX :: 로그, 환경변수 페이지 타이틀 증발

--- app/project/[projectId]/env/page.tsx
+++ app/project/[projectId]/env/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import EnvSection from "@/components/layout/section/env";
+import { headerTitleState } from "@/store/atoms/layout/headerTitle";
+import { ProjectType } from "@/types/project";
+import { getProject } from "@/utils/api/project";
 import { NextSeo, NextSeoProps } from "next-seo";
 import React from "react";
+import { useQuery } from "react-query";
+import { useSetRecoilState } from "recoil";
 
 interface EnvProps {
   params: {
@@ -15,6 +20,18 @@ function Env(props: EnvProps) {
     title: "프로젝트 환경변수",
     description: "프로젝트에 환경변수를 추가하는 페이지입니다.",
   };
+
+  const setTitle = useSetRecoilState(headerTitleState);
+
+  useQuery<ProjectType, Error>(
+    "project",
+    () => getProject(String(props.params.projectId)),
+    {
+      onSuccess: (data) => {
+        setTitle(data?.name);
+      },
+    }
+  );
 
   return (
     <>
--- app/project/[projectId]/log/page.tsx
+++ app/project/[projectId]/log/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import LogSection from "@/components/layout/section/log";
+import { headerTitleState } from "@/store/atoms/layout/headerTitle";
+import { ProjectType } from "@/types/project";
+import { getProject } from "@/utils/api/project";
 import { NextSeo, NextSeoProps } from "next-seo";
+import { useQuery } from "react-query";
+import { useSetRecoilState } from "recoil";
 
 interface LogProps {
   params: {
@@ -14,6 +19,18 @@ export default function Log(props: LogProps) {
     title: "프로젝트 로그",
     description: "프로젝트의 로그를 보는 페이지입니다.",
   };
+
+  const setTitle = useSetRecoilState(headerTitleState);
+
+  useQuery<ProjectType, Error>(
+    "project",
+    () => getProject(String(props.params.projectId)),
+    {
+      onSuccess: (data) => {
+        setTitle(data?.name);
+      },
+    }
+  );
 
   return (
     <>
