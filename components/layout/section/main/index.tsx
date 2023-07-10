e2ef4fe0b1a094a2b7dcdcd3f74c4e43a6137cfc
leehj050211 <leehj050211@gmail.com>
2023-07-10 23:04:26 +0900

    FEAT :: 사용 가이드 링크

--- components/layout/section/main/index.tsx
+++ components/layout/section/main/index.tsx
@@ -13,20 +13,29 @@ export default function Main() {
 
   return (
     <section className="main-section flex items-center justify-center flex-col font-bold">
-      <h1 className="text-[70px] mobile:text-[40px] mb-[100px]">BSM Deploy</h1>
+      <h1 className="text-[70px] mobile:text-[40px] mb-[50px]">BSM Deploy</h1>
       <p className="mb-[15px] text-[22.5px] mobile:text-[20px] mobile:w-[75%]">
-        BSM Deploy는 정적/동적 사이트의 배포를 도와주는 서비스입니다.
+        BSM Deploy는 부산소마고 재학생을 위한 웹사이트 배포 서비스입니다.
       </p>
       <p className="text-[22.5px] mobile:text-[20px] mobile:w-[75%]">
-        여러분들의 웹사이트 도메인 뒤에 bssm.kro.kr을 붙여보세요!
+        그동안 배포하지 못했던 프로젝트를 무료로 배포해보세요!
       </p>
-      <Link
-        href={"/setting"}
-        className="text-black w-[180px] h-[60px] mt-[120px] text-[22.5px] mobile:w-[130px] mobile:h-[45px] mobile:text-[15px] blue-button"
-      >
-        시작하기{" "}
-        <ArrowForwardIcon className="ml-[12.5px] w-[25px] h-[25px] mobile:w-[20px] mobile:h-[20px]" />
-      </Link>
+      <div className="flex gap-10">
+        <a
+          href="https://www.youtube.com/playlist?list=PL0Lb9XSDAE1fZvQp-ez37nIHMdQgu8OJs"
+          target="_blank"
+          className="!text-white w-[180px] h-[60px] mt-[100px] text-[22.5px] mobile:w-[130px] mobile:h-[45px] mobile:text-[15px] blue-button bg-deepGrayButton"
+        >
+          사용 가이드
+        </a>
+        <Link
+          href={"/setting"}
+          className="text-black w-[180px] h-[60px] mt-[100px] text-[22.5px] mobile:w-[130px] mobile:h-[45px] mobile:text-[15px] blue-button"
+        >
+          시작하기{" "}
+          <ArrowForwardIcon className="ml-[12.5px] w-[25px] h-[25px] mobile:w-[20px] mobile:h-[20px]" />
+        </Link>
+      </div>
     </section>
   );
 }
